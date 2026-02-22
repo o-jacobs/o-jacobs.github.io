573d91152ca830410ba0e9b11443d4b509c5923a
o-jacobs <objacobs@ncsu.edu>
2026-02-21 23:00:27 -0500

    Create general.js
    adding general.js javascript

---
+++ general.js
@@ -0,0 +1,12 @@
+//stuff for tabs------------------
+window.onscroll = function() {scrolllFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("myname").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("myname").style.fontSize = "35px";
+  }
+}
